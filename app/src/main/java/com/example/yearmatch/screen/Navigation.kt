999715be5786a2b40e39634ea14cc7d3ed2fbaa8
仲優樹 <yu-ki216m@outlook.jp>
2025-04-09 07:31:53 +0900

    Added other countries to NavHost.

--- app/src/main/java/com/example/yearmatch/screen/Navigation.kt
+++ app/src/main/java/com/example/yearmatch/screen/Navigation.kt
@@ -10,10 +10,22 @@ fun Navigation(navHostController: NavHostController) {
     NavHost(navHostController, startDestination = "countrySelection") {
         composable("countrySelection") { CountrySelectionScreen(navHostController) }
         composable("japanQuiz") { JapanQuizScreen(navHostController) }
-        composable("koreaQuiz") { KoreaQuizScreen(navHostController) }
         composable("chinaQuiz") { ChinaQuizScreen(navHostController) }
+        composable("koreaQuiz") { KoreaQuizScreen(navHostController) }
+        composable("russiaQuiz") { RussiaQuizScreen(navHostController) }
         composable("usaQuiz") { UsaQuizScreen(navHostController) }
         composable("ukQuiz") { UkQuizScreen(navHostController) }
-        composable("russiaQuiz") { RussiaQuizScreen(navHostController) }
+        composable("germanyQuiz") { UkQuizScreen(navHostController) }
+        composable("italyQuiz") { UkQuizScreen(navHostController) }
+        composable("netherlandsQuiz") { UkQuizScreen(navHostController) }
+        composable("portugalQuiz") { UkQuizScreen(navHostController) }
+        composable("spainQuiz") { UkQuizScreen(navHostController) }
+        composable("mongoliaQuiz") { UkQuizScreen(navHostController) }
+        composable("türkiyeQuiz") { UkQuizScreen(navHostController) }
+        composable("indiaQuiz") { UkQuizScreen(navHostController) }
+        composable("iranQuiz") { UkQuizScreen(navHostController) }
+        composable("greeceQuiz") { UkQuizScreen(navHostController) }
+        composable("iraqQuiz") { UkQuizScreen(navHostController) }
+        composable("egyptQuiz") { UkQuizScreen(navHostController) }
     }
 }
