2b1caf7893ac3a8ca448bfd7bf737bc20bcedb38
仲優樹 <yu-ki216m@outlook.jp>
2025-04-13 09:44:54 +0900

    Replaced with CenterAlignedTopAppBar.

--- app/src/main/java/com/example/yearmatch/screen/CountrySelectionScreen.kt
+++ app/src/main/java/com/example/yearmatch/screen/CountrySelectionScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CountrySelectionScreen(navController: NavHostController) {
     val countries = listOf(
@@ -35,30 +39,35 @@ fun CountrySelectionScreen(navController: NavHostController) {
         "🇪🇬" to "egyptQuiz"
     )
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "国を選択してください",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
-        )
-
-
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier.padding(16.dp)
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "国を選択してください")
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            items(countries.size) { index ->
-                val (flag, route) = countries[index]
-                Text(
-                    text = flag,
-                    fontSize = 100.sp, // 国旗を大きくする
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .clickable { navController.navigate(route) }
-                )
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.padding(16.dp)
+            ) {
+                items(countries.size) { index ->
+                    val (flag, route) = countries[index]
+                    Text(
+                        text = flag,
+                        fontSize = 100.sp, // 国旗を大きくする
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .clickable { navController.navigate(route) }
+                    )
+                }
             }
         }
     }
